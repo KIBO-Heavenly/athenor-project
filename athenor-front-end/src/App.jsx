92e5c47cfdc63477b3869a238d156b9bcf452c69
KIBO-Heavenly <pinongabriel292@gmail.com>
2025-10-30 23:18:49 -0500

    Reset repo: clean front-end and back-end structure

---
+++ athenor-front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./Login"; // make sure the path matches where you put Login.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
